8d4f834327a3dd294e0a97de24a10b81bd869feb
begarco <benoit.garcon@outlook.com>
2016-04-24 13:05:17 +0000

    second

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #screen -d -m -S minecraft java -d64 -server -Xms400M -Xmx600M -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+AggressiveOpts -jar minecraft_server.jar nogui
-screen -d -m -S minecraft nice -n -20 java -d64 -server -Xms600M -Xmx700M -jar minecraft_server.jar nogui
+screen -d -m -S minecraft nice -n -20 java -server -Xms500M -Xmx620M -jar minecraft_server.jar nogui
